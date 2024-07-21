2ce2a582a01f19bc686105113db5fd5de02f55ac
SutirthaSaha <sutirthasaha12@gmail.com>
2024-07-21 23:26:11 +0530

    composition over inheritance

--- cache/main.py
+++ cache/main.py
@@ -1,11 +1,12 @@
 from model import CacheFactory
 from enums import CacheType
 
-cache = CacheFactory(3).get_cache(CacheType.LRU)
+cache = CacheFactory(3).get_cache(CacheType.FIFO)
 cache.add("1", "1")
 cache.add("2", "2")
+cache.get("1")
 cache.add("3", "3")
 cache.add("4", "4")
 
 cache.get("1")
-print(cache.get("2"))
+cache.get("2")
--- cache/model.py
+++ cache/model.py
@@ -2,76 +2,81 @@ from abc import ABC, abstractmethod
 
 from util import DoublyLinkedList, ListNode
 from enums import CacheType
-from collections import deque
 
 
-class Cache(ABC):
-    def __init__(self, capacity):
+class EvictionPolicy(ABC):
+    def __init__(self):
+        self.list = DoublyLinkedList()
+        self.mapper = dict()
+
+    @abstractmethod
+    def access_key(self, key):
+        pass
+
+    @abstractmethod
+    def evict_key(self):
+        pass
+
+
+class LeastRecentlyUsed(EvictionPolicy):
+
+    def access_key(self, key):
+        if key in self.mapper:
+            node = self.mapper[key]
+            self.list.detach(node)
+        else:
+            node = ListNode(key)
+            self.mapper[key] = node
+        self.list.attach(node)
+
+    def evict_key(self):
+        node = self.list.head
+        self.list.detach(node)
+        self.mapper.pop(node.val)
+        return node.val
+
+
+class FirstInFirstOut(EvictionPolicy):
+
+    def access_key(self, key):
+        if key not in self.mapper:
+            node = ListNode(key)
+            self.mapper[key] = node
+            self.list.attach(node)
+
+    def evict_key(self):
+        node = self.list.head
+        self.list.detach(node)
+        self.mapper.pop(node.val)
+        return node.val
+
+
+class Cache:
+    def __init__(self, capacity, eviction_policy: EvictionPolicy):
         self.storage = dict()
         self.capacity = capacity
+        self.eviction_policy = eviction_policy
 
-    @abstractmethod
     def get(self, key) -> str:
-        pass
+        if key not in self.storage:
+            raise Exception("Key doesn't exist")
+        self.eviction_policy.access_key(key)
+        return self.storage[key]
 
-    @abstractmethod
     def add(self, key, value) -> bool:
-        pass
+        if self.is_storage_full():
+            self.remove()
+        self.storage[key] = value
+        self.eviction_policy.access_key(key)
 
-    @abstractmethod
     def remove(self) -> bool:
-        pass
+        key = self.eviction_policy.evict_key()
+        self.storage.pop(key)
 
     def is_storage_full(self) -> bool:
         if len(self.storage.keys()) == self.capacity:
             return True
         return False
-
-
-class LRUCache(Cache):
-    def __init__(self, capacity):
-        super().__init__(capacity)
-        self.list = DoublyLinkedList()
-        self.mapper = dict()
-
-    def get(self, key) -> str:
-        if key not in self.mapper:
-            raise Exception("Key doesn't exist")
-        node = self.mapper(key)
-        self.list.attach(node)
-        return self.storage[key]
-
-    def add(self, key, value) -> bool:
-        if self.is_storage_full():
-            self.remove()
-        self.storage[key] = value
-        node = ListNode(key)
-        self.list.attach(node)
-
-    def remove(self) -> bool:
-        node = self.list.head
-        self.list.detach(node)
-        self.mapper.pop(node.val)
-        return True
-
-
-class FIFOCache(Cache):
-    def __init__(self, capacity):
-        super().__init__(capacity)
-        self.order = deque()
-
-    def get(self, key) -> str:
-        return self.storage[key]
-
-    def add(self, key, value) -> bool:
-        if self.is_storage_full():
-            self.remove()
-        self.order.append(key)
-        self.storage[key] = value
-
-    def remove(self) -> bool:
-        key = self.order.popleft()
-        self.storage.pop(key)
 
 
 class CacheFactory:
@@ -81,6 +86,6 @@ class CacheFactory:
 
     def get_cache(self, cache_type: str):
         if cache_type == CacheType.LRU:
-            return LRUCache(self.capacity)
+            return Cache(self.capacity, LeastRecentlyUsed())
         elif cache_type == CacheType.FIFO:
-            return FIFOCache(self.capacity)
+            return Cache(self.capacity, FirstInFirstOut())
