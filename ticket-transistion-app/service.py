6df31086641af07045b43d8150025577931f8f4b
SutirthaSaha <sutirthasaha12@gmail.com>
2024-07-21 17:07:40 +0530

    add singletons for states

--- ticket-transistion-app/service.py
+++ ticket-transistion-app/service.py
@@ -8,25 +8,25 @@ class TicketService:
         self.lock = threading.Lock()
 
     def create_ticket(self, desc: str, user: User):
-        return Ticket(desc, user, Analysis())
+        return Ticket(desc, user, Analysis.get_instance())
 
     def start_analysis(self, ticket: Ticket):
         with self.lock:
             is_feasible = ticket.get_state().start_analysis(ticket)
             if is_feasible:
-                ticket.set_state(Analysis())
+                ticket.set_state(Analysis.get_instance())
 
     def start_review(self, ticket: Ticket):
         with self.lock:
             is_feasible = ticket.get_state().start_review(ticket)
             if is_feasible:
-                ticket.set_state(Review())
+                ticket.set_state(Review.get_instance())
 
     def mark_done(self, ticket: Ticket):
         with self.lock:
             is_feasible = ticket.get_state().mark_done(ticket)
             if is_feasible:
-                ticket.set_state(Done())
+                ticket.set_state(Done.get_instance())
 
     def change_ticket_state(self, ticket: Ticket, ticket_state: TicketState):
         if ticket_state == TicketState.ANALYSIS:
